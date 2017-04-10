7fd300a3f413ad905c575f6b105054f8f3a0fe78
akash-gupta-146 <ag.akash007@gmail.com>
2017-04-11 01:32:27 +0530

    Updated signup page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -5,7 +5,27 @@ import { VerifyPage } from './verify/verify';
 
 @Component({
   selector: 'page-signup',
-  templateUrl: 'signup.html'
+  templateUrl: 'signup.html',
+  styles: [`
+    .select-avatar{
+      border-top:3px solid black;
+      border-bottom:3px solid black;
+      overflow: auto;
+      white-space: nowrap;
+    }
+    .avatar-icon{
+      display: inline-block;
+      color: black;
+      font-size:2.2em;
+      text-align: center;
+      padding: 14px;
+      text-decoration: none;
+    }
+    .avatar-icon:hover{
+      background-color: #777;
+      color:white;
+    }
+  `]
 })
 
 export class SignupPage {
